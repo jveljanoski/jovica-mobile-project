e020da7cec15e45070a1b0b588f394e833db18cf
Jovica Veljanoski <jovicav@vestelglobal.com>
2024-11-18 20:22:23 +0100

    Added method to capture screenshots on test failure in Listeners

--- src/test/java/com/companyname/app/utils/Listeners.java
+++ src/test/java/com/companyname/app/utils/Listeners.java
@@ -1,4 +1,36 @@
 package com.companyname.app.utils;
 
-public class Listeners {
+import io.appium.java_client.AppiumDriver;
+import io.qameta.allure.Allure;
+import org.openqa.selenium.OutputType;
+import org.openqa.selenium.TakesScreenshot;
+import org.testng.ITestContext;
+import org.testng.ITestResult;
+import org.testng.TestListenerAdapter;
+import java.io.ByteArrayInputStream;
+import java.util.UUID;
+
+public class Listeners extends TestListenerAdapter {
+
+    @Override
+    public void onTestFailure(ITestResult result) {
+        System.out.println("Executing listener on failure");
+
+        // Retrieve Appium driver instance
+        AppiumDriver<?> driver = (AppiumDriver<?>) result.getTestContext().getAttribute("driver");
+
+        if (driver != null) {
+            try {
+                // Capture and attach screenshot to Allure report
+                Allure.addAttachment(
+                        "Screenshot on Failure - " + UUID.randomUUID(),
+                        new ByteArrayInputStream(((TakesScreenshot) driver).getScreenshotAs(OutputType.BYTES))
+                );
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+        } else {
+            System.err.println("Appium Driver instance is null. Screenshot cannot be taken.");
+        }
+    }
 }
